1bba6a92774cd4d6c852b57187544654cccf5e6e
salas <salas@trajano.us.es>
2024-02-05 18:11:36 +0100

    Otro cambio

--- lib/src/main/java/edu/gitt/is/magiclibrary/controller/MagicLibrary.java
+++ lib/src/main/java/edu/gitt/is/magiclibrary/controller/MagicLibrary.java
@@ -37,6 +37,7 @@ public class MagicLibrary {
 	public static void main(String[] args) {
 		
 		log.info("Hola soy xxx ejecutando la aplicacion");
+		log.info("Otro cambio");
 		log.info("Arranco la aplicaci�n y creo el controlador para manejar entidades desde la interfaz");
 		log.log(Level.INFO, "Arranco la aplicaci�n y creo el controlador para manejar entidades desde la interfaz");
 
